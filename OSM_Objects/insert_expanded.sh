693a1d2756d0664acb3bdf6f0f73598741297e22
Matteo Samadelli <matteo.samadelli@ost.ch>
2021-03-29 10:02:17 +0200

    applying changes from datendelphin to insert_expanded.sh

--- OSM_Objects/insert_expanded.sh
+++ OSM_Objects/insert_expanded.sh
@@ -23,15 +23,12 @@ osm_objects() {
     echo "Saving Changedata in $REPDIR/changes.osm.gz (using $REPDIR/sequence.state)"
     pyosmium-get-changes -f $REPDIR/sequence.state -o $REPDIR/changes.osm.gz
     echo "Creating Insertfiles from $REPDIR/changes.osm.gz in $REPDIR/"
-    ope -H -a $REPDIR/changes.osm.gz $REPDIR/nodes=n%I.v.d.c.t.i.T.x.y. $REPDIR/ways=w%I.v.d.c.t.i.T.N. $REPDIR/relations=r%I.v.d.c.t.i.T.M. $REPDIR/users=u%i.u.
-    echo "Editing sql Files"
-    sed -i 's/DROP TABLE IF EXISTS ".*" CASCADE;//' $REPDIR/*.sql
-    sed -i 's/(/IF NOT EXISTS (/' $REPDIR/*.sql
+    ope -H $REPDIR/changes.osm.gz $REPDIR/nodes=n%I.v.d.c.t.i.T.Gp $REPDIR/ways=w%I.v.d.c.t.i.T.N. $REPDIR/relations=r%I.v.d.c.t.i.T.M. $REPDIR/users=u%i.u.
     echo "Writing Data to Database $DB"
-    psql -d $DB -f $REPDIR/nodes.sql
-    psql -d $DB -f $REPDIR/ways.sql
-    psql -d $DB -f $REPDIR/relations.sql
-    psql -d $DB -f $REPDIR/users.sql
+    psql -d $DB -c "\\copy \"nodes\" from '$REPDIR/nodes.pgcopy'"
+    psql -d $DB -c "\\copy \"ways\" from '$REPDIR/ways.pgcopy'"
+    psql -d $DB -c "\\copy \"relations\" from '$REPDIR/relations.pgcopy'"
+    psql -d $DB -c "\\copy \"users\" from '$REPDIR/users.pgcopy'"
     echo "Starting $REPDIR/osm_pg_db_clipper.py"
     python3 $REPDIR/osm_pg_db_clipper.py -d $DB -b $REPDIR/borders.geojson
     echo "Deleting changefiles in $REPDIR/"
@@ -48,3 +45,4 @@ main() {
 }
 
 main
+
