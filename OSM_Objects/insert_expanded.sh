230937eb8eb5b6fb915c58590d70adc8d59a60e0
Lukas Buchli <lukas.buchli@ost.ch>
2021-09-06 10:57:07 +0200

    Allow for primary keys in database

--- OSM_Objects/insert_expanded.sh
+++ OSM_Objects/insert_expanded.sh
@@ -5,32 +5,31 @@
 # sequence.state added in it. ope and changesetmd.py must either be added to the
 # path variable or the path must be added here.
 
-REPDIR="replication"
-DB="changesets"
+REPDIR="/tmp/osmhistory-replication"
+DB="osmhistory"
 
 mkdir -p $REPDIR
-exec &> $REPDIR/insert_logs.txt
+exec &> insert_logs.txt
 set -e
 
 changesets() {
     echo "Save new Changesets into Database"
-    python3 changesetmd.py -d $DB -r -g
+    python2 /home/lukas/workspace/ChangesetMD/changesetmd.py -d $DB -r -g
     echo "Deleting Changesets outside Switzerland"
-    python3 osm_changeset_deleter.py -d $DB -b $REPDIR/borders.geojson
+    python3 osm_changeset_deleter.py -d $DB -b borders.geojson
 }
 
 osm_objects() {
-    echo "Saving Changedata in $REPDIR/changes.osm.gz (using $REPDIR/sequence.state)"
-    pyosmium-get-changes -f $REPDIR/sequence.state -o $REPDIR/changes.osm.gz
+    echo "Saving Changedata in $REPDIR/changes.osm.gz (using sequence.state)"
+    pyosmium-get-changes -f sequence.state -o $REPDIR/changes.osm.gz
     echo "Creating Insertfiles from $REPDIR/changes.osm.gz in $REPDIR/"
     ope -H $REPDIR/changes.osm.gz $REPDIR/nodes=n%I.v.d.c.t.i.T.Gp $REPDIR/ways=w%I.v.d.c.t.i.T.N. $REPDIR/relations=r%I.v.d.c.t.i.T.M. $REPDIR/users=u%i.u.
+    echo "Setting permissions for repdir"
+    chmod -R 755 $REPDIR
     echo "Writing Data to Database $DB"
-    psql -d $DB -c "\\copy \"nodes\" from '$REPDIR/nodes.pgcopy'"
-    psql -d $DB -c "\\copy \"ways\" from '$REPDIR/ways.pgcopy'"
-    psql -d $DB -c "\\copy \"relations\" from '$REPDIR/relations.pgcopy'"
-    psql -d $DB -c "\\copy \"users\" from '$REPDIR/users.pgcopy'"
-    echo "Starting $REPDIR/osm_pg_db_clipper.py"
-    python3 $REPDIR/osm_pg_db_clipper.py -d $DB -b $REPDIR/borders.geojson -f $REPDIR/changes.osm.gz
+    psql -d $DB -f import.sql
+    echo "Starting osm_pg_db_clipper.py"
+    python3 osm_pg_db_clipper.py -d $DB -b borders.geojson -f $REPDIR/changes.osm.gz
     echo "Deleting changefiles in $REPDIR/"
     rm $REPDIR/changes.osm.gz
     rm $REPDIR/ways.*
